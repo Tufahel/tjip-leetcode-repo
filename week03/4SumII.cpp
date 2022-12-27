5d40df974f50e175369e3b75e3791c24c61d2a3c
Tufahel <tufahel96@gmail.com>
2022-12-28 01:45:41 +0600

    Update TC & MC for 4sum and Isomorphic

--- week03/4SumII.cpp
+++ week03/4SumII.cpp
@@ -1,6 +1,8 @@
 class Solution {
 public:
     int fourSumCount(vector<int>& nums1, vector<int>& nums2, vector<int>& nums3, vector<int>& nums4) {
+        // TC: O(N^2), N = nums.length
+        // MC: O(N^2), N^2 = nums1.length * nums2.length
         unordered_map<int, int> pairwiseSum;
         for(auto &x : nums1){
             for(auto &y : nums2){
--- week03/IsomorphicStrings.cpp
+++ week03/IsomorphicStrings.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     bool isIsomorphic(string s, string t) {
         // TC: O(N), N = String Length;
-        // MC: O(1), 520 bits = 65 bytes
+        // MC: O(Size), Size = 520 bits = 65 bytes
         int hashValueforS[260] = { 0 }, hashValueforT[260] = { 0 };
         for(int index = 0; index < s.length(); index++){
             int m = int(s[index]), n = int(t[index]);
